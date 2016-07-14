f532f4a391745cf9eb20da3470b63b38aa6d9864
hiepnn91 <hiepnn1991@gmail.com>
2016-07-15 01:25:45 +0700

    Xóa comment

--- app/src/main/java/com/example/hiepnn/mydemoapplication/MainActivity.java
+++ app/src/main/java/com/example/hiepnn/mydemoapplication/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with my action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with my great action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -30,23 +30,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
