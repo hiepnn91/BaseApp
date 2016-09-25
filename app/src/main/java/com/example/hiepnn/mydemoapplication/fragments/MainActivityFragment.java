0839e7201d1f7654457be6f72d960b4937ff9e3c
HiepNN <hiepnn@1link.vn>
2016-09-25 07:51:10 +0700

    commit test

---
+++ app/src/main/java/com/example/hiepnn/mydemoapplication/fragments/MainActivityFragment.java
@@ -0,0 +1,21 @@
+package com.example.hiepnn.mydemoapplication.fragments;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hiepnn.mydemoapplication.R;
+
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
