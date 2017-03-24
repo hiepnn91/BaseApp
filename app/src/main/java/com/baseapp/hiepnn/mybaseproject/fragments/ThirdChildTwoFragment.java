ffc36ec6bc9360f78d6a23478051757580ce1f13
hiepn <hiepnn1991@gmail.com>
2017-03-24 09:50:25 +0700

    delete test

--- app/src/main/java/com/baseapp/hiepnn/mybaseproject/fragments/ThirdChildTwoFragment.java
+++ app/src/main/java/com/baseapp/hiepnn/mybaseproject/fragments/ThirdChildTwoFragment.java
@@ -1,11 +1,13 @@
 package com.baseapp.hiepnn.mybaseproject.fragments;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.baseapp.hiepnn.mybaseproject.R;
+import com.baseapp.hiepnn.mybaseproject.utils.FragmentUtil;
 
 /**
  * Created by hiepn on 23/03/2017.
@@ -30,5 +32,20 @@ public class ThirdChildTwoFragment extends BaseFragment {
     @Override
     protected void initData() {
 
+    }
+
+    @Override
+    protected void processCustomToolbar() {
+        FragmentUtil.popBackStack(this);
+    }
+
+    @Override
+    protected void processOnBackPress() {
+        FragmentUtil.popBackStack(this);
+    }
+
+    @Override
+    protected Drawable getIconLeft() {
+        return getResources().getDrawable(R.drawable.ic_back);
     }
 }
